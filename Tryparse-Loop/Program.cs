9a933a6e6c408bb30200193095f0f919dbff0c13
theo lindqvist <theo.lindqvist@elev.ga.ntig.se>
2023-05-23 10:48:52 +0200

    Möjlighet att hitta genomsnittet av float-värden

--- Tryparse-Loop/Program.cs
+++ Tryparse-Loop/Program.cs
@@ -1,4 +1,4 @@
-﻿List<int> numbers = new();
+﻿List<float> numbers = new();
 
 Console.WriteLine("Hur många nummer vill du hitta ett genomsnitt av? ");
 string antalStr = Console.ReadLine();
@@ -6,7 +6,7 @@ bool success = int.TryParse(antalStr, out int antalInt);
 
 while (!success || antalInt < 1)
 {
-    Console.WriteLine("Du måste skriva en siffra (heltal)");
+    Console.WriteLine("Du måste skriva en siffra (heltal) som är större än 0");
     Console.WriteLine("Hur många nummer vill du hitta ett genomsnitt av? ");
     antalStr = Console.ReadLine();
     success = int.TryParse(antalStr, out antalInt);
@@ -16,15 +16,15 @@ for (var i = 0; i < antalInt; i++)
 {
     Console.WriteLine($"Nummer {i + 1}: ");
     string listAddString = Console.ReadLine();
-    bool listAddSuccess = int.TryParse(listAddString, out int listAddInt);
+    bool listAddSuccess = float.TryParse(listAddString, out float listAddFloat);
     while (!listAddSuccess)
     {
-        Console.WriteLine("Du måste skriva en siffra (heltal)");
+        Console.WriteLine("Du måste skriva en siffra (vid decimaltal, formatera såhär: 7,5)");
         Console.WriteLine($"Nummer {i + 1}: ");
         listAddString = Console.ReadLine();
-        listAddSuccess = int.TryParse(listAddString, out listAddInt);
+        listAddSuccess = float.TryParse(listAddString, out listAddFloat);
     }
-    numbers.Add(listAddInt);
+    numbers.Add(listAddFloat);
 }
 
 Console.WriteLine($"Genomsnittet av dina siffror är {numbers.Average()}");
